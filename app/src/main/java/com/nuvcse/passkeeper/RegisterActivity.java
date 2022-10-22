5f419303a4adade5e4c850dfbe9c517686f704d6
ruchitc <ruchit.contractor@gmail.com>
2022-10-22 19:46:16 +0530

    firebase sdk added and minor changes to xml

--- app/src/main/java/com/nuvcse/passkeeper/RegisterActivity.java
+++ app/src/main/java/com/nuvcse/passkeeper/RegisterActivity.java
@@ -3,12 +3,31 @@ package com.nuvcse.passkeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText mUsername, mEmail, mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mUsername = findViewById(R.id.edit_text_username);
+        mEmail = findViewById(R.id.edit_text_email);
+        mPassword = findViewById(R.id.edit_text_password);
+        mRegisterBtn = findViewById(R.id.button_register);
+        progressBar = findViewById(R.id.progress_register);
+
+        fAuth = FirebaseAuth.getInstance();
     }
 }
