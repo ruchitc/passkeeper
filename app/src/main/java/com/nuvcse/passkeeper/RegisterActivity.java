d739f286bdf33bb88c62f00dc807fb5c19e24f3a
ruchitc <ruchit.contractor@gmail.com>
2022-10-22 23:38:42 +0530

    registration functionality added

--- app/src/main/java/com/nuvcse/passkeeper/RegisterActivity.java
+++ app/src/main/java/com/nuvcse/passkeeper/RegisterActivity.java
@@ -1,13 +1,21 @@
 package com.nuvcse.passkeeper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -29,5 +37,48 @@ public class RegisterActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_register);
 
         fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)) {
+                    mEmail.setError("Enter your email.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)) {
+                    mPassword.setError("Enter the password");
+                    return;
+                }
+
+                if(password.length() < 6) {
+                    mPassword.setError("Password must be at least 6 characters long.");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // Registering to database
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this,"Account created.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
